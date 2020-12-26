0e6f0217efab7a1ab6c7b1bd7a17a8044db5413f
Ilya <balashovia@bk.ru>
2020-12-27 01:11:50 +0300

    Lessson2

--- hw1/js/main.js
+++ hw1/js/main.js
@@ -1,4 +1,4 @@
-const products = [
+const newProducts = [
     {id: 1, title: 'GoPro7 Black', price: 22000},
     {id: 2, title: 'GoPro8 Black', price: 27000},
     {id: 3, title: 'GoPro9 Black', price: 35000},
@@ -9,19 +9,82 @@ const products = [
     {id: 8, title: 'GoPro7 White', price: 18000},
     {id: 9, title: 'GoPro9 Silver', price: 30000}
 ];
-//Функция для формирования верстки каждого товара
-const renderProduct = product => {
-    return `<div class="productItem">
-                <div><img class="productImage" src="pictures/nophoto.png" alt="photo"></div>
-                <h3 class="productName">${product.title}</h3>
-                <p class="productPrice">${(product.price == undefined)? 777: product.price} руб.</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item)).join('');
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
-};
 
-renderPage(products);
+
+class Product{
+    constructor(product){
+        this.id = product.id;
+        this.title = product.title;
+        this.price = product.price;
+    }
+    
+    showInfo(){
+        // выводит инфо о продукте
+    }
+}
+
+class ProductItem extends Product{    
+        renderProduct(){     
+            return `<div class="productItem">
+                <div><img class="productImage" src=${(this.img == undefined)? "pictures/nophoto.png": this.img} alt="photo"></div>
+                <h3 class="productName">${this.title}</h3>
+                <p class="productPrice">${(this.price == undefined)? 777: this.price} руб.</p>
+                <button class="buy-btn">Купить</button>
+            </div>`           
+        }        
+	}
+
+class Catalog{
+    constructor(products, container = '.products'){ 
+        this.container = container;       
+        this.products = products;                
+    }
+    renderPage(){
+        const block = document.querySelector(this.container);        
+            for (let item of this.products){            
+            const productObj = new ProductItem(item);            
+            block.insertAdjacentHTML('beforeend',productObj.renderProduct())
+        }
+    } 
+}
+
+class Basket{
+    constructor(productsInBasket){          
+        this.products = productsInBasket;                
+    }
+
+    renderBasket(){
+        // для отрисовки корзины
+    }
+
+    removeProduct(){
+        // удаляет элемент из корзины
+    }
+
+    summaryBasket(){               
+        let sum = 0;
+        for (let item of this.products){            
+            sum += item.price
+        }
+        return sum;
+    }   
+}
+
+class ProductInBasket extends Product{
+    constructor(product, quantity=1){
+        super(product);
+        this.quantity = quantity;        
+    }
+        //помимо стандартных свойств добавилось количество данного продукта в корзине, по умолчанию - 1
+}
+
+
+// let list = new Catalog(newProducts);
+// list.renderPage();
+
+
+// let bask = new Basket(newProducts);
+// console.log(bask.summaryBasket());
+
+// let el = new ProductInBasket({id: 1, title: 'GoPro7 Black', price: 22000});
+// console.log(el)
